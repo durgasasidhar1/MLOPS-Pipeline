2c0057641181bf3e7531984847068418e3bf1ec5
durgasasidhar1 <sasidharnandikolla@gmail.com>
2025-07-06 23:09:10 +0530

    Pipeline configured using DVC without params

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -69,7 +69,7 @@ def save_data(train_data:pd.DataFrame, test_data:pd.DataFrame, data_path:str) ->
 
 def main():
     try:
-        test_size = 0.2
+        test_size = 0.3
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
